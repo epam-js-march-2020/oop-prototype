1c07b268d6a9b9e475881ce0bef08d0c2423683c
krivoruchkoND <lizey@bk.ru>
2020-05-01 14:53:55 +0300

    better formatting

--- src/views/app.js
+++ src/views/app.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 
 var app = _.template(
   `<div class="row">
-      <div class="col-5">
+      <div class="col-3" id="menuSection">
           <%=menu()%>
       </div>
-      <div class="col-4">
-          <%=options()%>
+      <div class="col-5" id="optionsSection">
+          <%=options%>
       </div>
-      <div class="col-3">
+      <div class="col-4" id="cartSection">
           <%=cart()%>
       </div>
   </div>`
