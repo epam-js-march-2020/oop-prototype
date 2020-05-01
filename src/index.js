3bb0ddbc8b30fac23cf5efc1895cdd9d08e283c6
krivoruchkoND <lizey@bk.ru>
2020-05-01 14:53:43 +0300

    basic logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello Project.');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+
+import app from './views/app';
+import options from './views/options';
+import menu from './views/menu';
+import cart from './views/cart';
+
+function outlineActiveCard(card) {
+  $('.card').removeClass('border-success');
+  $(card).addClass('border-success');
+}
+
+$('#app').append(app({ menu, options: options('init'), cart }));
+$('#burgersCard').on('click', function() {
+  $('#optionsSection').empty().append(options('burgers'));
+  outlineActiveCard(this);
+});
+$('#saladsCard').on('click', function() {
+  $('#optionsSection').empty().append(options('salads'));
+  outlineActiveCard(this);
+});
+$('#drinksCard').on('click', function() {
+  $('#optionsSection').empty().append(options('drinks'));
+  outlineActiveCard(this);
+});
+
