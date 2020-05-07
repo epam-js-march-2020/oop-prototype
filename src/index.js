6d1250751eb936099897055b49007ab6c22e4860
Marianna Minich <marry-minich@ld.yandex.ru>
2020-05-07 23:58:41 +0300

    add total price

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 
 //MENU
-function Menu(type, quantity) {
+function Menu(type, quantity = 1) {
     this.type = type;
     this.quantity = quantity;
     this.name = type.name;
@@ -42,12 +42,7 @@ function Hamburger(size, stuffing, quantity) {
     this.name = size.name + ' burger ' + stuffing.name;
   };
 
-// function Hamburger(type, stuffing, quantity) {
-//   Menu.call(this, type, quantity);
-//   this.stuffing = stuffing;
-//   //this.name = size.name + ' hamburger ' + stuffing.name;
-//   //this.name = size.name + ' hamburger ';
-// };
+
 
 
   Hamburger.prototype = Object.create(Menu.prototype);
@@ -112,7 +107,7 @@ function getFullHamburgerOrder() {
    console.log('stuffingDataValue', stuffingDataValue) //obj {type: "STUFFING_CHEESE", stuffing: "Cheese", priceStuffing: "10", caloryStuffing: "20"}
 
    hamburgerName.textContent = sizeDataValue.size + ' hamburger with' + ' ' + stuffingDataValue.stuffing
-   hamburgerSum.textContent = parseInt(sizeDataValue.price, 10) + parseInt(stuffingDataValue.priceStuffing, 10) * hamburgerCount.value;
+   hamburgerSum.textContent = parseInt(sizeDataValue.price, 10) * hamburgerCount.value + parseInt(stuffingDataValue.priceStuffing, 10) * hamburgerCount.value + ' tu';
    hamburgerCalory.textContent = (parseInt(sizeDataValue.calory, 10) + parseInt(stuffingDataValue.caloryStuffing, 10)) * hamburgerCount.value +  ' cal';;
  }
       
@@ -141,8 +136,6 @@ hamburgerAddBtn.addEventListener('click', function(e) {
 
 
 
-
-
 //SALAD
 
 
@@ -151,10 +144,8 @@ function Salad(type, quantity) {
   };
 
 
-  //console.log('Salad', new Salad('cesar', 5)) //{type: "cesar", quantity: 5, name: undefined}
   Salad.prototype = Object.create(Menu.prototype);
-  //console.log('Salad.prototype', Salad.prototype)
-
+  
 
   Salad.prototype.constructor = Salad;
 
@@ -178,7 +169,7 @@ Salad.CAESAR = {
     };
     Drink.prototype = Object.create(Menu.prototype);
     Drink.prototype.constructor = Drink;
-    //console.log('Drink.prototype', Drink.prototype);
+    
 
     Drink.COLA = {
       name: 'Cola',
@@ -236,12 +227,18 @@ Salad.CAESAR = {
         // }, [])
         this.items = this.items.filter((item, i) => {
           return i !== Number(el)
-         //return el.indexOf == i;
+        
         })
      } else {
         console.log('позиция уже оплачена')
      }
  }
+
+
+ Order.prototype.deleteAllDishes = function () {
+  this.items = [];
+  this.isPaid = false;
+}
 
 
  Order.prototype.calcTotalPrice = function () {
@@ -254,23 +251,23 @@ Salad.CAESAR = {
            total +=item.calculatePrice();
            return total;
         }, 0)
-      //return total;
+      
      }
    //return total;
  };
 
  Order.prototype.calcTotalCalories = function () { 
-    //var totalCal = 0;
+    
     if (this.items.length == 0) {
         console.log('нет позиций')
-        //return total
+        
     } else {
        return this.items.reduce((totalCal, item) => {
         totalCal +=item.calculateCalories() //получаем метод из родительского МЕНЮ
         return totalCal;
        }, 0)
     }
-    // return totalCal;
+    
  }
 
 
@@ -278,8 +275,6 @@ Salad.CAESAR = {
  //create order
 
  var container = document.querySelector('.content') // контейнер с карточками 
-
-
  var order = new Order()
 
  var saladSection = document.querySelector('.content__item-salad');
@@ -301,24 +296,21 @@ Salad.CAESAR = {
     
    var weight = saladSection.querySelector('.form__input-quantity').value;
 
-//Salad(type, quantity) 
+
    var salad = new Salad(Salad[type], weight / 100) //создаем объект салата
 
    //добавляем в заказ:
    order.addDishes(salad);
    getOrderData();
-   //total order
    calcTotalOrder();
 
  })
 
  function getFullSaladOrder () {
     var { salad, price, calory } = saladSection.querySelector('input:checked').dataset
-    //console.log('type SALAD:', { salad, price, calory }) //{type: "OLIVIE", price: "50", calory: "80"}
 
     saladName.textContent = salad;
-    //console.log('saladName.textContent', saladName.textContent);
-    saladSum.textContent = price + ' tu';
+    saladSum.textContent = (price * saladCount.value) / 100 + ' tu';
     saladCalory.textContent = (calory * saladCount.value) / 100 + ' cal';
  }
 
@@ -342,16 +334,16 @@ Salad.CAESAR = {
  drinksAddBtn.addEventListener('click', function(e) {
    e.preventDefault()
     var { type } = drinksSection.querySelector('input:checked').dataset;
-    //console.log('type:', type)
-   var count = drinksSection.querySelector('.form__input-quantity').value;
+   
+    var count = drinksSection.querySelector('.form__input-quantity').value;
 
-//Salad(type, quantity) 
+
    var drink = new Drink(Drink[type], count) //создаем объект салата
-   //console.log('drink', drink)
+  
    //добавляем в заказ:
    order.addDishes(drink);
    getOrderData();
-   //total order
+   
    calcTotalOrder()
 
  })
@@ -362,64 +354,9 @@ Salad.CAESAR = {
     var {drink, price, calory} = drinksSection.querySelector('input:checked').dataset;
 
     drinkName.textContent = drink;
-    drinkSum.textContent = price + ' tu';
+    drinkSum.textContent = (price * drinkCount.value) + ' tu';
     drinkCalory.textContent = (calory * drinkCount.value) +  ' cal';
  }
-
-
-
-//  //собрать гамбургер
-//  var hamburgerSection = document.querySelector('.content__item-hamburger');
-//  var hamburgerName = hamburgerSection.querySelector('.form__total-item-name');
-//  var hamburgerSum = hamburgerSection.querySelector('.form__total-item-sum');
-//  var hamburgerCalory = hamburgerSection.querySelector('.form__total-item-calories');
-//  var hamburgerCount = hamburgerSection.querySelector('.form__input-quantity');
-//  var hamburgerAddBtn = hamburgerSection.querySelector('.form__add-btn');
-
-//  hamburgerSection.addEventListener('change', function() {
-//     getFullHamburgerOrder()
-//  });
-
-
-
-// function getFullHamburgerOrder() {
-
-//   var sizeData = hamburgerSection.querySelector('[name=hamburger-size]:checked')
-//   var stuffingData = hamburgerSection.querySelector('[name=hamburger-stuffing]:checked')
-//   var hamburgerCount = hamburgerSection.querySelector('.form__input-quantity');
-//   if (sizeData && stuffingData) {
-//     var stuffingDataValue = stuffingData.dataset;
-//     var sizeDataValue = sizeData.dataset;
-//     hamburgerName.textContent = sizeDataValue.size + ' hamburger with' + ' ' + stuffingDataValue.stuffing
-//     hamburgerSum.textContent = parseInt(sizeDataValue.price, 10) + parseInt(stuffingDataValue.priceStuffing, 10) * hamburgerCount.value;
-//     hamburgerCalory.textContent = (parseInt(sizeDataValue.calory, 10) + parseInt(stuffingDataValue.caloryStuffing, 10)) * hamburgerCount.value +  ' cal';;
-//   }
-       
-
-// }
-
-
-
-
-// hamburgerAddBtn.addEventListener('click', function(e) {
-//   e.preventDefault()
-//   // var dataSize = hamburgerSection.querySelector('[name=hamburger-size]:checked').dataset;
-//   // var dataStuffing = hamburgerSection.querySelector('[name=hamburger-stuffing]:checked').dataset
-//   // var quantity = hamburgerSection.querySelector('.form__input-quantity').value;
-
-
-//   // console.log('dataSize', dataSize) //{type: "SIZE_SMALL", size: "Small", price: "50", calory: "20"}
-//   // console.log('dataStuffing', dataStuffing) //{type: "STUFFING_CHEESE", stuffing: "Cheese", priceStuffing: "10", caloryStuffing: "20"}
-//   // console.log('quantity', quantity)
-//   // if(dataSize && dataStuffing) {
-//   //   var hamburger = new Hamburger(Hamburger[dataSize.type], quantity, Hamburger[dataStuffing.type])
-//   //   console.log('HAMBURGER', hamburger);
-//   //   order.addDishes(hamburger);
-//   //   getOrderData();
-//   // calcTotalOrder()
-//   // }
-//  })
-
 
 
 
@@ -430,7 +367,8 @@ var orderList = orderContainer.querySelector('.order__list');
 var totalSum = orderContainer.querySelector('.order__sum');
 var totalCalories = orderContainer.querySelector('.order__calory');
 var value = document.querySelectorAll('.order__sum--value')
-
+var orderTitle = document.querySelector('.order__subtitle');
+var orderdetails = document.querySelector('.order__wrapper')
 
 //собираем данные
 function getOrderData() {
@@ -443,31 +381,37 @@ function getOrderData() {
     var delBtn = document.createElement('button');
     delBtn.classList.add('order__list-delete-item');
     delBtn.textContent = 'X';
-    // console.log('orderItem', orderItem);
-    var position = item.name + ' ' + item.quantity +  ', ' + 'PRICE: ' + parseInt(item.type.price, 10) * parseInt(item.quantity, 10) + ' ' + ' tu' + ' TOTAL CALORIES: ' + parseInt(item.type.calories, 10) * parseInt(item.quantity) + ' cal';
+   
+    var position = item.name + ' ' + item.quantity +  ', ' + 'PRICE: ' + parseInt(item.type.price, 10) * parseInt(item.quantity, 10) + ' ' + ' tu' + ', TOTAL CALORIES: ' + parseInt(item.type.calories, 10) * parseInt(item.quantity) + ' cal';
     if(item.stuffing) {
-      var position = item.name + ' ' + item.quantity +  ', ' + 'PRICE: ' + (parseInt(item.type.price, 10) + parseInt(item.stuffing.price, 10)) * parseInt(item.quantity, 10) + '' + ' tu' + ' TOTAL CALORIES: ' + (parseInt(item.type.calories, 10) + (parseInt(item.stuffing.calories, 10))) * parseInt(item.quantity) + ' cal';
+      var position = item.name + ' ' + item.quantity +  ', ' + 'PRICE: ' + (parseInt(item.type.price, 10) + parseInt(item.stuffing.price, 10)) * parseInt(item.quantity, 10) + '' + ' tu' + ', TOTAL CALORIES: ' + (parseInt(item.type.calories, 10) + (parseInt(item.stuffing.calories, 10))) * parseInt(item.quantity) + ' cal';
     }
     orderItem.textContent = position;
     orderItem.setAttribute('id', index)
     orderItem.appendChild(delBtn);
+
+    orderItem.classList.add('active');
+    orderTitle.classList.add('active');
+    orderdetails.classList.add('active');
+    payBtn.classList.remove('disabled')
+    payBtn.removeAttribute('disabled')
     orderList.appendChild(orderItem);
   });
+
+  if(orderArray.length ==0) {
+    orderTitle.classList.remove('active');
+    orderdetails.classList.remove('active');
+    payBtn.classList.add('disabled')
+    payBtn.setAttribute("disabled", "disabled")
+  }
 }
 
-//используем делегирование при удалении позиции из заказа:
-// orderContainer.addEventListener('click', function(event) {
-//   var orderItem = document.querySelector('.order__list-item')
-//   if(event.target.closest('.order').querySelector('.order__list-delete-item')) {
-//     var targetEl = event.target.closest('.order').querySelector('.order__list-item');
-//     orderList.removeChild(event.target.closest('.order').querySelector('.order__list-item'));
-//     getOrderData()
-    
-//   }
-// });
+
+
+
 orderContainer.addEventListener('click', function(event) {
   var orderItem = document.querySelector('.order__list-item');
-  // console.log('orderItem.id', orderItem.id)
+
   if(event.target.classList.contains('order__list-delete-item')) {
     var targetEl = event.target.closest('.order__list-item');
     
@@ -488,11 +432,10 @@ orderContainer.addEventListener('click', function(event) {
 
 
 orderContainer.addEventListener('click', function(event) {
-  //console.log('event.target', event.target)
   if(event.target.classList.contains('order__pay')) {
-     //console.log('event.target')
     order.getItems().length !==0 ? order.payOrder() : console.log('нельзя оплатить пустой заказ')
   }
+
 });
 
 
@@ -501,25 +444,38 @@ orderContainer.addEventListener('click', function(event) {
 var calcTotalOrder = function () {
   totalCalories.textContent = order.calcTotalCalories();
   totalSum.textContent = order.calcTotalPrice();
+
+  var orderArray = order.getItems();
+  orderArray.forEach(function(item,  index) {
+    if(item.stuffing) {
+      totalSum.textContent = order.calcTotalPrice() + (parseInt(item.stuffing.price, 10) * parseInt(item.quantity, 10))
+      totalCalories.textContent = order.calcTotalCalories() + (parseInt(item.stuffing.calories, 10) * parseInt(item.quantity));
+    }
+  })
+  
 }
 
 
 function resetInputs() {
   var inputsArr = document.querySelectorAll('input:checked');
+  var countsInput = document.querySelectorAll('.form__input-quantity')
   inputsArr.forEach(function(input) {
     input.checked = false;
   })
-  // for (var i = 0; i < input.length; i++) {
-  //   input[i].checked = false
-//   }
+  countsInput.forEach(function(item) {
+    item.value = 0
+  })
 };
 
 var newOrderBtn = document.querySelector('.order__new');
+
 console.log('add', newOrderBtn)
 
 newOrderBtn.addEventListener('click', function() {
-  orderList.remove()
+  //orderList.remove()
+  order.deleteAllDishes();
   resetInputs();
+  getOrderData()
   totalSum.textContent = 0;
   totalCalories.textContent = 0;
 })
